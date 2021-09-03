7462bb8c92dfc2654ee71a7a9affa1f0425e56ef
Bruna Campos <92brunacampos@gmail.com>
2021-09-03 01:25:29 -0300

    desafio 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,38 @@
+
+db.air_alliances.aggregate([
+  {
+    $unwind: "$airlines",
+  },
+  {
+    $lookup: {
+      from: "air_routes",
+      localField: "airlines",
+      foreignField: "airline.name",
+      as: "airlineRoutes",
+    },
+  },
+  {
+    $unwind: "$airlineRoutes",
+  },
+  {
+    $match: {
+      "airlineRoutes.airplane": {
+        $in: ["747", "380"],
+      },
+    },
+  },
+  {
+    $group: {
+      _id: "$name",
+      totalRotas: { $sum: 1 },
+    },
+  },
+  {
+    $sort: {
+      totalRotas: -1,
+    },
+  },
+  {
+    $limit: 1,
+  },
+]);
