f3d5c1920322063ac40ad36e4e0cf212e3da3f9d
Bruna Campos <92brunacampos@gmail.com>
2021-09-02 22:45:32 -0300

    desafio 5

---
+++ challenges/desafio5.js
@@ -0,0 +1,32 @@
+const favAct = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", "George Clooney"];
+
+db.movies.aggregate([
+  {
+    $match: {
+      countries: "USA",
+      "tomatoes.viewer.rating": { $gte: 3 },
+      cast: { $in: favAct },
+    },
+  },
+  {
+    $addFields: {
+      num_favs: { $size: {
+        $setIntersection: [favAct, "$cast"],
+      },
+      },
+    },
+  },
+  {
+    $sort: {
+      num_favs: -1, "tomatoes.viewer.rating": -1, title: -1,
+    },
+  },
+  {
+    $project: {
+      _id: 0,
+      title: 1,
+    },
+  },
+  { $limit: 25 },
+  { $skip: 24 },
+]);
